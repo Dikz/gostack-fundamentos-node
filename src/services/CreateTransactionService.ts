3f8ffa05d1488472e58496bfc84490f834c6aac8
Dikz <cicerogabriel12@hotmail.com>
2020-05-09 00:49:55 -0300

    Done :)

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,32 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (typeof value !== 'number') {
+      throw new Error('The value must be a number');
+    }
+
+    if (Math.sign(value) !== 1) {
+      throw new Error('The value must be a positive number');
+    }
+
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('The type of the transaction is invalid');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new Error("You don't have enough balance");
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
